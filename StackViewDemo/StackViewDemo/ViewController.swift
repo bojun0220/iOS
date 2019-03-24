ec76eb0e4ef74bde0a13f5568770904bcac156b1
bojun <b04505041@ntu.edu.tw>
2019-03-24 17:07:18 +0800

    Stack View Demo

---
+++ StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by bojun  Huang on 2019/3/24.
+//  Copyright © 2019 NTU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
