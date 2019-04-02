5bf09f40436f57298da146d9fccfe139da11e774
bojun <b04505041@ntu.edu.tw>
2019-04-02 11:03:08 +0800

    Calculator

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by bojun  Huang on 2019/3/28.
+//  Copyright © 2019 NTU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    var first = [0, 0]
+    var later = [0, 0]
+    var calcbutton = false
+    var dotbutton = false
+    var calcway: Character = "+"
+
+    
+    @IBOutlet var numButton: [UIButton]!
+    @IBAction func pushNum(_ sender: UIButton) {
+            if let number = numButton.firstIndex(of: sender){
+                if calcbutton == false{
+                    if dotbutton == false{
+                        first[0] = first[0] * 10 + number
+                    }else{
+                        first[1] = first[1] * 10 + number
+                    }
+                }else{
+                    if dotbutton == false{
+                        later[0] = later[0] * 10 + number
+                    }else{
+                        later[1] = later[1] * 10 + number
+                    }
+                }
+            }else{
+            }
+    }
+    
+    @IBAction func dot(_ sender: UIButton) {
+        dotbutton = true
+    }
+    func calculate(first: String, second: String, operation: Character)->String{
+        var answer = 0.0
+        
+        switch operation {
+        case "+":
+            if let firstnum = Double(first), let secondnum = Double(second){
+                answer = firstnum + secondnum
+            }
+        case "-":
+            if let firstnum = Double(first), let secondnum = Double(second){
+                answer = firstnum - secondnum
+            }
+        case "*":
+            if let firstnum = Double(first), let secondnum = Double(second){
+                answer = firstnum * secondnum
+            }
+        case "/":
+            if let firstnum = Double(first), let secondnum = Double(second){
+                answer = firstnum / secondnum
+            }
+        default:
+            return first
+        }
+            return String(answer)
+    }
+    @IBAction func equal(_ sender: UIButton) {
+        if calcbutton == false{
+            //print(first)
+            showlabel.text = first
+        }else{
+            //print(calculate(first: first, second: later, operation: calcway))
+            showlabel.text = calculate(first: first, second: later, operation: calcway)
+        }
+        first = ""
+        later = ""
+        calcbutton = false
+        calcway = "+"
+
+        
+    }
+    @IBAction func plus(_ sender: UIButton) {
+        if calcbutton == false{
+            calcbutton = true
+            calcway = "+"
+            showlabel.text = "+"
+        }else{
+            
+        }
+    }
+    @IBAction func minus(_ sender: UIButton) {
+        if calcbutton == false{
+            calcbutton = true
+            calcway = "-"
+            showlabel.text = "-"
+        }else{
+            
+        }
+    }
+    @IBAction func times(_ sender: UIButton) {
+        if calcbutton == false{
+            calcbutton = true
+            calcway = "*"
+            showlabel.text = "×"
+        }else{
+            
+        }
+    }
+    @IBAction func divide(_ sender: UIButton) {
+        if calcbutton == false{
+            calcbutton = true
+            calcway = "/"
+            showlabel.text = "÷"
+        }else{
+            
+        }
+    }
+    @IBAction func clear(_ sender: UIButton) {
+        first = ""
+        later = ""
+        calcbutton = false
+        calcway = "+"
+        showlabel.text = "0"
+    }
+    @IBAction func plusminus(_ sender: UIButton) {
+    }
+    @IBAction func mod(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var showlabel: UILabel!
+    
+}
+
--- test1/test1/ViewController.swift
+++ test1/test1/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Magic.setTitleColor(.red, for: .normal)
+        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
     @IBAction func showMessage(sender: UIButton) {
@@ -19,7 +21,22 @@ class ViewController: UIViewController {
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
-
+    var temp = true
+    @IBOutlet weak var Magic: UIButton!
+    @IBAction func magicaction(_ sender: Any) {
+        temp = !temp
+        updateUI()
+    }
+    func updateUI(){
+        if temp{
+            view.backgroundColor = .white
+            Magic.setTitle("Off", for: .normal)
+        }else{
+            view.backgroundColor = .black
+            Magic.setTitle("On", for: .normal)
+        }
+    }
+    
 
 }
 
